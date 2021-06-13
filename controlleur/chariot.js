55865a5bda9505a760484b5f6cc59f831afa9d45
aziznaffati <aziznaffati9@gmail.com>
2021-06-13 01:47:02 +0100

    hahahha

--- controlleur/chariot.js
+++ controlleur/chariot.js
@@ -34,7 +34,7 @@ export const getChariot = async (req, res) => {
 export const getC = async (req, res) => {
   const { snC } = req.params;
   try {
-    const Chariot = await Chariot.find({ snC: snC });
+    const Chariot = await Chariot.findOne({ snC: snC });
     res.status(200).json(Chariot);
   } catch (error) {
     res.status(404).json({ message: error.message });
